2e5ec0703e2966850d789e6ecb08508f8112f160
Kings Blessing Diane <blessingkings104@gmail.com>
2023-02-16 05:27:58 -0800

    Task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include  <stdio.h>
+/*
+ * main function - print a line of code using puts
+ * return value - 0
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
